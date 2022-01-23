e3a7f10ca67893b49b4b442264b855ab81371a8b
Shalah <zoungranaala@gmail.com>
2022-01-22 21:18:27 -0500

    Rearranged my code to make it work by moving most functions under a single one.

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,7 @@
 // Assignment Code
 // console.log("Check if everything is linked")
 
-var lowerAlphabets = "abcdefghijklmnopqrstuvwxyz";
-var upperAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var symbols = "!@#$%^&*(),./;{}[]<>";
-var usedCharacters = "";
-var userInputLength = "";
-var randomPassword = "";
+
 
 var generateBtn = document.querySelector("#generate");
 
@@ -15,50 +9,13 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  passwordText.value = password;
   // console.log(userInputLength);
 
-  for(let i=0; i <= userInputLength; i++){
-    randomPassword += usedCharacters.charAt(Math.floor(Math.random() * userInputLength[i]))
-    //console.log(userInputLength);
-
-    return randomPassword;
-    console.log(password);
-  }
-  password = randomPassword;
-  console.log(password);
-  console.log(randomPassword);
-  passwordText.value = password;
 
 }
 
-function trackingUsedCharacters(){
-  if (userInputLength < 8 || userInputLength > 129){
-    alert("")
-  }
-}
 
-function test(){
-  // Using "Number()" will convert the string into number.
-  var userInput = Number(prompt("How many characters would you like your password to contain?"))
-  console.log(userInput)
-  console.log(typeof userInput)
-
-  if (userInput < 8){ 
-    alert("Password length must be at least 8")
-    test();
-  }
-  else if (userInput > 129){
-    alert("Password length must less than 129")
-    test();
-  }
-  else{
-    userInputLength = userInput;
-    confirmedCharacters();
-  }
-
-  return;
-}
 
 
 function confirmedCharacters(){
@@ -94,34 +51,61 @@ function confirmedCharacters(){
 
 //Added this
 function generatePassword(){
-  var password = "";
+  var lowerAlphabets = "abcdefghijklmnopqrstuvwxyz";
+  var upperAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var symbols = "!@#$%^&*(),./;{}[]<>";
+  var usedCharacters = "";
+  var userInputLength = "";
+  var randomPassword = "";
+
+  var userInput = Number(prompt("How many characters would you like your password to contain?"))
+  console.log(userInput)
+  console.log(typeof userInput)
+
+  if (userInput < 8){ 
+    alert("Password length must be at least 8")
+    
+  }
+  if (userInput > 129){
+    alert("Password length must less than 129")
+
+  }
+  else{
+    userInputLength = userInput;
+  }
   
-  test();
+
+  if (confirm("Click OK to confirm adding lowercase characters") == true) {
+    usedCharacters += lowerAlphabets;
+    console.log(usedCharacters)
+  } 
+  if (confirm("Click OK to confirm adding uppercase characters") == true) {
+    usedCharacters += upperAlphabets;
+    console.log(usedCharacters)
+  } 
+  if (confirm("Click OK to confirm adding numeric characters") == true) {
+    usedCharacters += numbers;
+    console.log(usedCharacters)
+  } 
+  if (confirm("Click OK to confirm adding special characters") == true) {
+    usedCharacters += symbols;
+    console.log(usedCharacters)
+  } 
+  if (usedCharacters == 0 ){
+    alert("You have to pick one option at least")
+  }
+  else{
+    for(let i=0; i < userInput; i++){
+      randomPassword += usedCharacters.charAt(Math.floor(Math.random() * usedCharacters.length))
+      //console.log(userInputLength);
+    }
+    console.log(randomPassword);
+    return randomPassword;
+  }
+
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// document.getElementById("generate").addEventListener("click", test);
-
-
-
-/* 
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-
-*/
-
